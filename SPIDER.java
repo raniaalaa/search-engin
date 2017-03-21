c77990784f35c3a4ece2fb5329715218bdaa4a17
raniaalaa <rania.alaaa@yahoo.com>
2017-03-22 01:17:38 +0200

    Add files via upload

--- SPIDER.java
+++ SPIDER.java
@@ -1,11 +1,11 @@
-//package project;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.Statement;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+
 
 //import com.mysql.fabric.Response;
 import java.sql.Connection;
@@ -41,15 +41,13 @@ public class SPIDER implements Runnable{
         // t.start();
         //  this.conn=conn;
         urlarray=new ArrayList<String>();
-
         urlarray.add("https://en.wikipedia.org/wiki/Main_Page");
-
         urlarray.add("http://stackoverflow.com/");
         urlarray.add("http://w3schools.com/");
         urlarray.add("https://www.tutorialspoint.com/");
         urlarray.add("http://illinois.edu/");
         urlarray.add("https://opensource.org/");
-        urlarray.add("http://www.dmoz.org/");
+        // urlarray.add("http://www.dmoz.org/");
         urlarray.add("http://marketwatch.com/");
         urlarray.add("http://usnews.com/");
         urlarray.add("https://www.amazon.com/");
@@ -68,54 +66,53 @@ public class SPIDER implements Runnable{
     }
     public  void DoNotCrawl(String url) throws SQLException
     {//Connection conn1 = null;
-     try{   String str,robot,temp;
-        str=robot=temp="";
-        InputStream inputStream;
-        temp=url+"/robots.txt";
-        //URL myURL=new URL(url+"/robots.txt");
-        URL myURL = new URL(temp);
-        inputStream=myURL.openStream();
-        InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
-        BufferedReader in = new BufferedReader(inputStreamReader);
-        String arr[];
-        str=in.readLine();
-         if(str!=null)
-        str= str.toLowerCase();
-        if(str!=null && str.contains("user-agent: *"))
-        {
-            while(str!=null)
+        try{   String str,robot,temp;
+            str=robot=temp="";
+            InputStream inputStream;
+            temp=url+"/robots.txt";
+            //URL myURL=new URL(url+"/robots.txt");
+            URL myURL = new URL(temp);
+            inputStream=myURL.openStream();
+            InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
+            BufferedReader in = new BufferedReader(inputStreamReader);
+            String arr[];
+            str=in.readLine();
+            if(str!=null)
+                str= str.toLowerCase();
+            if(str!=null && str.contains("user-agent: *"))
             {
-                if(str.contains("disallow:"))
+                while(str!=null)
                 {
-                    arr=str.split(" ");
-                    if(arr.length<1)
-                        continue;
-                    robot=url+arr[1];
-                   // System.out.println(robot);
-                    //insert in the DB
-                    synchronized (this) {
-                    Statement st = db.conn.createStatement();
-                    boolean flag=db.Check_Restrictedurl(robot);
-                    if(flag) {
-                        String query = "INSERT INTO `restrictedurls`(`ulr`, `id`) VALUES ('" + robot + "',NULL)";
+                    if(str.contains("disallow:"))
+                    {
+                        arr=str.split(" ");
+                        if(arr.length<2)
+                            return;
+                        robot=url+arr[1];
+                        // System.out.println(robot);
+                        //insert in the DB
+                        Statement st = db.conn.createStatement();
+                        boolean flag=db.Check_Restrictedurl(robot);
+                        if(flag) {
+                            String query = "INSERT INTO `restrictedurls`(`ulr`, `id`) VALUES ('" + robot + "',NULL)";
 
                             st.executeUpdate(query);
                         }
+
                     }
+                    robot+=str;
+                    str=in.readLine();
+                    if(str!=null)
+                        str= str.toLowerCase();
                 }
-                robot+=str;
-                str=in.readLine();
-                if(str!=null)
-                    str= str.toLowerCase();
+
             }
+        }
+        catch(IOException e)
+        {
+            return;
 
         }
-     }
-     catch(IOException e)
-     {
-         return;
-
-     }
 
 
     }
@@ -124,10 +121,9 @@ public class SPIDER implements Runnable{
 
         String sql="SELECT id FROM `restrictedurls` WHERE ulr='"+url+"'";
         ResultSet rs;
-        synchronized (this)
-        {Statement sta = db.conn.createStatement();
+        Statement sta = db.conn.createStatement();
         rs = sta.executeQuery(sql);
-        }
+
         if(!rs.isBeforeFirst())
         {
 
@@ -146,93 +142,111 @@ public class SPIDER implements Runnable{
         else return false;
     }
     public void Crawl(String url) throws SQLException, IOException
-    {
-        doc = Jsoup.connect(url).timeout(0).userAgent(useragent).get();
+    {if(url!=null&&!url.equals(""))
+    { doc = Jsoup.connect(url).timeout(4000).userAgent(useragent).get();
+        if(!doc.html().equals(""))
+        {    db.UpdateDoc(url,doc.html());
+            // System.out.println("doc done ");
+            db.Updatefile(url);
+
+        }
+       // System.out.println("jsoup done");
         org.jsoup.select.Elements links=doc.select("a[href]");
         int id=Integer.parseInt(Thread.currentThread().getName());
         boolean flag3=IsHTML(doc.html());
-        synchronized (this)
-        {
-            db.UpdateVisted(url);
-        }
-            boolean flag1,flag2;
+
+        db.UpdateVisted(url);
+
+        boolean flag1,flag2;
         for(Element e: links)
 
         {  String tmpurl=e.attr("abs:href");
             DoNotCrawl(tmpurl);
-            //System.out.println(e.attr("abs:href"));
-            synchronized (this)
-            {
-             flag1=db.Check_Exist(tmpurl);
-            }
-             flag2=CheckRobotDisallow(tmpurl);
+         //   System.out.println("dont crawl");
 
+            flag1=db.Check_Exist(tmpurl);
+
+            flag2=CheckRobotDisallow(tmpurl);
+          //  System.out.println("CheckRobotDisallow ");
             if(flag1&flag2&flag3)
-            { synchronized (this) {
+            {
                 db.Insert(tmpurl, "");
+            //    System.out.println("insert ");
             }
-            }
-         //   else System.out.print(" thread number" +id+" link exist in db ");
+            //   else System.out.print(" thread number" +id+" link exist in db ");
 
 
         }
-        synchronized (this)
-        {
-            db.UpdateDoc(url,doc.html());
-            db.Updatefile(url);
-        }
-        //	}
 
+    }
     }
 
     @Override
     public void run() {
         // TODO Auto-generated method stub
-       // System.out.print("run ");
+      //  System.out.print("run ");
         String url="";
-
         int id=Integer.parseInt(Thread.currentThread().getName());
-        url=urlarray.get(Integer.parseInt(Thread.currentThread().getName()));
-        try {//System.out.print("try");
+     //   url=urlarray.get(Integer.parseInt(Thread.currentThread().getName()));
         int count=0;
-            while(count<10){
+        String currentUrl="";
+        try {
+          //  System.out.print("r");
+            count = db.GetCount();
+        } catch (SQLException e1) {
+            // TODO Auto-generated catch
+            //System.out.print("rundsd ");
+            e1.printStackTrace();
+        }
+        while(count<5000){
+            try {//System.out.print("try");
                 Statement st=db.conn.createStatement();
 
                 ResultSet result;
 
                 String q="select * from record";
-                synchronized (this)
-                {
-
-                    result=st.executeQuery(q);
-                }
-
-
-
-                String currentUrl;
-
+                result=st.executeQuery(q);
 
                 if(!result.next())// if empty
                 {
                     currentUrl = url;
-                    Crawl(currentUrl);
-
                 }
                 else
                 {
                     currentUrl = db.GetURL(number,id);
-                    Crawl(currentUrl);
                 }
-               count =db.GetCount();
-
+                Crawl(currentUrl);
+                count =db.GetCount();
             }
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            catch (SQLException |IOException e ) {
+                // TODO Auto-generated catch block
+                System.out.println("invalid url ");
+                try {
+                    db.SetInvalid(currentUrl);
+                } catch (SQLException e1) {
+                   // e1.printStackTrace();
+                }
+                //  e.printStackTrace();
+               /* try {
+                    //int v,f;
+                    //v=db.Getvisit(currentUrl);
+                    //f=db.Getfile(currentUrl);
+                    //if(v==1&&f==0)
+                      //  db.ResetVisted(currentUrl);
+                    continue;
+                } catch (SQLException e1) {
+                    // TODO Auto-generated catch block
+                   // e1.printStackTrace();
+                    System.out.println("invalid ResetVisted ");
+                }*/
+                continue;
+                //e.printStackTrace();
+            } //catch (IOException e) {
+            //System.out.print("invalid url ");
 
+            //e.printStackTrace();
+            //}
+        }
 
     }
 
