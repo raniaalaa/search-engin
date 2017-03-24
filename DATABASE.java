898ce9596c36e3f117b6c4aeaae5cdbee702c705
Menna33 <mennalotfy33@gmail.com>
2017-03-25 01:38:08 +0200

    Add files via upload

--- DATABASE.java
+++ DATABASE.java
@@ -11,8 +11,14 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.io.*;
 import java.sql.Statement;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+
+import org.apache.commons.lang3.Pair;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.net.MalformedURLException;
@@ -48,36 +54,37 @@ public class DATABASE {
         String sql,url;
         Statement sta;
         ResultSet rs;
-        String[] query={"DELETE FROM `expressionspositions` WHERE 1;","DELETE FROM `expressions` WHERE 1;","DELETE FROM `wordpositions` WHERE 1;","DELETE FROM `word` WHERE 1;","DELETE FROM `record` WHERE 1;","DELETE FROM `restrictedurls` WHERE 1;","ALTER TABLE `record` AUTO_INCREMENT = 1;"};
+        String[] query={"DELETE FROM `expressionscounts` WHERE 1;","DELETE FROM `expressions` WHERE 1;","DELETE FROM `wordpositions` WHERE 1;","DELETE FROM `word` WHERE 1;","DELETE FROM `record` WHERE 1;","DELETE FROM `restrictedurls` WHERE 1;","DELETE FROM `wordpositions` WHERE 1","DELETE FROM `word` WHERE 1","ALTER TABLE `record` AUTO_INCREMENT = 1;","ALTER TABLE `word` AUTO_INCREMENT = 1;"};
         //,"INSERT INTO `searchengine`.`record` (`ID`, `URL`, `document`, `Visted`, `file`,`invalid`) VALUES (NULL, 'http://resource-zone.com', '', '0', '0','0'),(NULL, 'http://en.wikipedia.org/wiki/main_page', '', '0', '0','0'), (NULL, 'http://stackoverflow.com', '', '0', '0','0'), (NULL, 'http://nature.com/index.html', '', '0', '0','0'), (NULL, 'http://w3.org', '', '0', '0','0'), (NULL, 'http://dmoztools.net', '', '0', '0','0'), (NULL, 'http://unicef.org', '', '0', '0','0') ,(NULL, 'http://ibm.com/us-en', '', '0', '0','0');"};
-        for(int i=0;i<=6;i++)
+        for(int i=0;i<=9;i++)
         {
-	        sql=query[i];
-	        sta = conn.createStatement();
-	        sta.execute(sql);
+            sql=query[i];
+            sta = conn.createStatement();
+            sta.execute(sql);
         }
         for(int i=0;i<arr.size();i++)
         {
-        	url=Normalize(arr.get(i));
-	        sql="INSERT INTO `searchengine`.`record` (`ID`, `URL`, `document`, `Visted`, `file`,`invalid`) VALUES (NULL,'"+url+"', '', '0', '0','0')";
-	        sta = conn.createStatement();
-	        sta.execute(sql);
+            url=Normalize(arr.get(i));
+            sql="INSERT INTO `searchengine`.`record` (`ID`, `URL`, `document`, `Visted`, `file`,`invalid`) VALUES (NULL,'"+url+"', '', '0', '0','0')";
+            sta = conn.createStatement();
+            sta.execute(sql);
         }
         return;
     }
+
     public String Normalize(String url) throws MalformedURLException
     {
-    	URL u = new URL(url.toLowerCase());
- 		url = u.getNormalizedUrl();
- 		if(url.equals(""))
- 			return url;
- 		else
- 	 		url = "http://"+url;
- 		return url;
+        URL u = new URL(url.toLowerCase());
+        url = u.getNormalizedUrl();
+        if(url.equals(""))
+            return url;
+        else
+            url = "http://"+url;
+        return url;
     }
-    public boolean Check_Exist(String url) throws SQLException, MalformedURLException {
-    	if(url.equals(""))
- 			return true;
+    public synchronized boolean Check_Exist(String url) throws SQLException, MalformedURLException {
+        if(url.equals(""))
+            return true;
         String sql="SELECT id FROM `record` WHERE url='"+url+"'";
         Statement sta = conn.createStatement();
         ResultSet rs = sta.executeQuery(sql);
@@ -88,6 +95,7 @@ public class DATABASE {
             return false;
         }
     }
+
     public boolean Check_Restrictedurl(String url) throws SQLException {
         String sql="SELECT id FROM `restrictedurls` WHERE ulr='"+url+"'";
         Statement sta = conn.createStatement();
@@ -99,17 +107,21 @@ public class DATABASE {
             return false;
         }
     }
-    public void Insert(String url,String text) throws SQLException, MalformedURLException {
-
-        if(url.equals(""))
-            return;
-        url=Normalize(url);
-        if(url.equals(""))
-            return;
-        String sql="INSERT INTO `record`(`ID`, `URL`, `document`,  `Visted`, `file`, `invalid`) VALUES (NULL,'"+url+"','"+text+"','0','0','0');";
-        //"insert into record(`ID`, `URL`, `Visted`,'file') VALUES (NULL,'"+url+"', '0','0') ";
-        Statement sta = conn.createStatement();
-        sta.execute(sql);
+    public synchronized void Insert(String url,String text) throws SQLException, MalformedURLException {
+        int iid=Integer.parseInt(Thread.currentThread().getName());
+       // System.out.println("insert thread "+iid+" "+url);
+        boolean check=Check_Exist(url);
+        if(check)
+        { if(url.equals(""))
+                return;
+            url=Normalize(url);
+            if(url.equals(""))
+                return;
+            String sql="INSERT INTO `record`(`ID`, `URL`, `document`,  `Visted`, `file`, `invalid`) VALUES (NULL,'"+url+"','"+text+"','0','0','0');";
+            //"insert into record(`ID`, `URL`, `Visted`,'file') VALUES (NULL,'"+url+"', '0','0') ";
+            Statement sta = conn.createStatement();
+            sta.execute(sql);
+        }
     }
 
     public synchronized void UpdateVisted(String url) throws SQLException, MalformedURLException {
@@ -117,19 +129,26 @@ public class DATABASE {
         Statement sta = conn.createStatement();
         sta.execute(sql);
     }
-    
-    
+
+
     //////////
-    public void UpdateDoc(String url,String text) throws SQLException {
+
+    public synchronized void UpdateDoc(String url,String text) throws SQLException {
 
         if(!text.equals("")) {
             try {
                 int id = Getid(url);
-                String filename = id + ".html";
+
+
+
+                if(id!=0)
+                {String filename = "html/"+id + ".html";
                 File myfile = new File(filename);
                 FileWriter writer = new FileWriter(myfile, true);
                 writer.write(text);
                 writer.close();
+//              int iid=Integer.parseInt(Thread.currentThread().getName());
+              System.out.println("thread update doc num "+id +" url=  "+url);}
             }
 
             catch (IOException e) {
@@ -137,11 +156,12 @@ public class DATABASE {
             }
 
         }
-        
+
     }
-    public void Updatefile(String url) throws SQLException, MalformedURLException {
+    public  synchronized void Updatefile(String url) throws SQLException, MalformedURLException {
         String sql="UPDATE record SET `file` = '1' WHERE `record`.`url` ='"+url+"'";
         Statement sta = conn.createStatement();
+      //  System.out.println("file set  " +url);
         sta.execute(sql);
     }
 
@@ -153,21 +173,40 @@ public class DATABASE {
         int value=0;
         if (rs.next()) {
             value= Integer.parseInt(rs.getString(1));
-          //  System.out.println(value);
-            }
+            //  System.out.println(value);
+        }
         return value;
     }
-    public int Getid(String url) throws SQLException {
 
-    	String sql="SELECT id FROM `record` WHERE url='"+url+"'";
+    public  synchronized int Getid(String url) throws SQLException {
+
+        String sql="SELECT id FROM `record` WHERE url='"+url+"'";
         Statement sta = conn.createStatement();
         ResultSet rs = sta.executeQuery(sql);
         int value=0;
         if (rs.next()) {
             value= Integer.parseInt(rs.getString(1));
             int id=Integer.parseInt(Thread.currentThread().getName());
-            System.out.println("thread num"+id +" "+value);}
+           // System.out.println("thread num"+id +" "+value);
+        }
         return value;
+    }
+    public boolean Getfile(String url) throws SQLException {
+
+        String sql="SELECT file FROM `record` WHERE url='"+url+"'";
+        Statement sta = conn.createStatement();
+        ResultSet rs = sta.executeQuery(sql);
+        int value=0;
+        if (rs.next()) {
+            value= Integer.parseInt(rs.getString(1));
+            int id=Integer.parseInt(Thread.currentThread().getName());
+            // System.out.println("thread num"+id +" "+value);
+
+
+        }
+        if(value==1)
+            return true;
+        return false;
     }
     public void SetInvalid(String url) throws SQLException {
         String sql="UPDATE `record` SET `invalid`=1 WHERE URL ='"+url+"'";
@@ -176,25 +215,25 @@ public class DATABASE {
     }
     public synchronized String GetURL(int n,int ind) throws SQLException, MalformedURLException {
 
-        String sql="SELECT url FROM record where visted =0 and file=0 and invalid=0 LIMIT 1";
+        String sql="SELECT url FROM record where visted =0 and invalid=0 LIMIT 1";
         Statement sta = conn.createStatement();
         String value="";
 
         ResultSet resultSet=sta.executeQuery(sql);
-      
+
         if (resultSet.next()) {
-        	//URL url = new URL(resultSet.getString(1).toLowerCase());
-    		//System.out.println("http://"+url.getNormalizedUrl());  
-    		value = resultSet.getString(1);
-          //  System.out.println(value);
-           // break;
+            //URL url = new URL(resultSet.getString(1).toLowerCase());
+            //System.out.println("http://"+url.getNormalizedUrl());
+            value = resultSet.getString(1);
+            //  System.out.println(value);
+            // break;
         }
-            //  for (int i = ind-1; i < columnsNumber; i++)
+        //  for (int i = ind-1; i < columnsNumber; i++)
 //            if(count==ind)
-               
-            	
-  //          count++;
-       
+
+
+        //          count++;
+
         // if (i > 1) System.out.print(",  ");
                 /*String columnValue = resultSet.getString(i);
                 System.out.print(columnValue + " " + rsmd.getColumnName(i));
@@ -291,7 +330,53 @@ public class DATABASE {
         else
             return -1;
     }
-
+    
+    public static void InsWords(Map<Pair,String> words,long DocID) throws Exception
+    {
+        PreparedStatement statement;
+        long WID;
+        for (Map.Entry<Pair, String> entry: words.entrySet())
+        {
+            Pair P = entry.getKey();   //P.left->word     //P.right-?importance
+            /*System.out.println((String)P.left);
+            System.out.println((int)P.right);*/
+            
+            
+            String position = entry.getValue(); 
+            //System.out.println(position);
+            if(!CheckWordExistance((String)P.left))
+        {
+        statement = conn.prepareStatement("insert into `Word` (`word`,`Wid`,`Stemming`) values('"+(String)P.left+"',NULL,NULL)");
+        statement.execute();
+        }
+        WID=GetWordID((String)P.left);
+        statement = conn.prepareStatement("insert into `wordpositions` (`w_id`,`doc_id`,`position`,`importance`) values("+WID+","+DocID+",'"+position+"','"+(int)P.right+"')");
+        statement.execute();
+        }	
+    }
+    public static void InsExpressions(Map<Pair, Integer> expressions,long DocID) throws Exception
+    {
+    	//Pair.left->l expression,Pair.right->importance,integer->count
+        PreparedStatement statement;
+        long EID;
+        
+        for (Entry<Pair, Integer> entry: expressions.entrySet())
+        {
+            Pair P = entry.getKey();   //P.left->word     //P.right->importance
+            /*System.out.println((String)P.left);
+            System.out.println((int)P.right);*/
+            int count = entry.getValue(); 
+           // System.out.println(count);
+            if(!CheckExpressionExistance((String)P.left))
+        { 		
+        statement = conn.prepareStatement("insert into `expressions` (`Expression`,`E_ID`) values('"+(String)P.left+"',NULL)");
+        statement.execute();
+        }
+        EID=GetExpressionID((String)P.left);
+        statement = conn.prepareStatement("insert into `expressionscounts` (`Expression_id`,`Doc_id`,`importance`,`count`) values("+EID+","+DocID+",'"+(int)P.right+"','"+count+"')");
+        statement.execute();
+        }	
+    }
    /* public  void DoNotCrawl(String url) throws SQLException, IOException
     {//Connection conn1 = null;
         String str,robot,temp;
