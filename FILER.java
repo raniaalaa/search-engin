a362c6d1f6e8f2d7d2b1f878905381c4b84cc395
Menna33 <mennalotfy33@gmail.com>
2017-03-25 23:18:37 +0200

    Add files via upload

--- FILER.java
+++ FILER.java
@@ -1,24 +1,10 @@
-import java.io.File;
-import javax.swing.text.Document;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-
-
+//class to dealing with files
 public class FILER {
 	private static  String Text;
 	
@@ -26,55 +12,49 @@ public class FILER {
 	{
 		Text="";
 	}
-    public static String GetText()
+    public static String GetText()   //function to return the text of the document
     {
     	return Text;
     }
-	public static  String [] Dealing_Files(File f) throws IOException
+	public static  String [] Dealing_Files(File f) throws IOException  //return array of important strings in the file
 	{ 
 		Text="";
 		String [] Importants={"","",""}; //first element is the title,second is all headers,third is img alt
 		org.jsoup.nodes.Document doc=Jsoup.parse(f, "UTF-8");
-		Importants[0]=doc.title();
-		
-			 String tag="h";
-			   String All_Headers="";
-			   Elements Header;
-			   
-			   for(int i=1;i<20;i++)
-				{  
-				   tag="h"+String.valueOf(i);
-				   Header = doc.select(tag);
-				if(Header.size()>0)
-					{
+		Importants[0]=doc.title(); //get the title of the file
+		Text=Text+" "+doc.title(); 
+	    String tag="h";
+	    String All_Headers="";
+		Elements Header;
+	    for(int i=1;i<20;i++) //loop to get text with headers tag of the file
+		 {  
+		   tag="h"+String.valueOf(i);
+		   Header = doc.select(tag);
+		   if(Header.size()>0)
+			{
 				Header= doc.getElementsByTag(tag);
 				String pConcatenated="";
-				        for (Element x: Header) {
-				            pConcatenated+= x.text()+" ";
-				            System.out.println("pConcatenated  "+pConcatenated);
-				        }
-				        All_Headers=All_Headers+pConcatenated;
-				        System.out.println("All_Headers "+All_Headers);
-					}
-				else
+				for (Element x: Header) 
+				{
+				         pConcatenated+= x.text()+" ";
+				}
+				All_Headers=All_Headers+pConcatenated;
+			}
+			else
 					break;
 				
-				}
-			   Importants[1]=All_Headers;
-			  Text=Text+" "+doc.text();
-			  String imgRegex = "<img[^>]+src\\s*=\\s*['\"]([^'\"]+)['\"][^>]*>";
-				Elements img = doc.getElementsByTag("img");
-				   for (Element element : img) 
-				   {
-			            if(element.attr("alt") != null && !(element.attr("alt").equals("")))
-			            {
-			            	Text=Text+" "+element.attr("alt");
-			            	Importants[2]=Importants[2]+" "+element.attr("alt");
-			            }
-			        }
-				  
-			   System.out.println("text  "+Text);
-			
+		 }
+	     Importants[1]=All_Headers;
+		 Text=Text+" "+doc.text();  //get the text of the document
+		 Elements img = doc.getElementsByTag("img"); //get the text with img tag 
+		 for (Element element : img) 
+		 {
+			if(element.attr("alt") != null && !(element.attr("alt").equals("")))
+			   {
+			     Text=Text+" "+element.attr("alt");
+			     Importants[2]=Importants[2]+" "+element.attr("alt");
+			   }
+	      }
        return Importants;
 	}
 
